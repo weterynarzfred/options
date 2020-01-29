6698b4d30dacf0c939a95e740b6da303ee896a8d
weterynarzfred <weterynarzfred@gmail.com>
2020-01-29 16:43:19 +0100

    get selected

--- options.js
+++ options.js
@@ -81,15 +81,20 @@ const options = {
 };
 
 function clone(object) {
-  return JSON.parse(JSON.encode(object));
+  return JSON.parse(JSON.stringify(object));
 }
 
 function getOption(path) {
-  if (typeof path === 'Object') return path;
+  if (typeof path === 'object') return path;
   path = path.split('/').reverse();
   let option = {options};
   while (path.length) {
-    option = option.options[path.pop()];
+    if (typeof option.selected === 'object' && option.selected[path[path.length - 1]] !== undefined) {
+      option = option.selected[path.pop()];
+    }
+    else {
+      option = option.options[path.pop()];
+    }
   }
   return option;
 }
@@ -110,10 +115,11 @@ function buyOption(option) {
     }
     const child = {
       type: 'child',
-      name: '',
+      name: option.name + ' - ' + option.selected.length,
       options: clone(option.individualOptions),
     };
     option.selected.push(child);
+    return child;
   }
   else {
     if (option.selected === undefined) {
@@ -123,6 +129,38 @@ function buyOption(option) {
   }
 }
 
-console.log(getCount('general/immortality'));
+function getSelected(options) {
+  const selected = [];
+  for (const slug in options) {
+    console.log(slug);
+    
+    const option = options[slug];
+    if (option.type !== 'group') {
+      if (option.selected !== undefined) {
+        if (typeof option.selected === 'object') {
+          for (const child of option.selected) {
+            selected.push(child);
+            selected.push(...getSelected(child.options));
+          }
+        }
+        else {
+          for (let i = 0; i < option.selected; i++) {
+            selected.push(option);
+          }
+        }
+      }
+    }
+    if (option.options !== undefined) {
+      selected.push(...getSelected(option.options));
+    }
+  }
+  return selected;
+}
+
 buyOption('general/immortality');
-console.log(getCount('general/immortality'));
+const plane1 = buyOption('planes');
+buyOption('planes');
+buyOption('planes/1/essence');
+buyOption('planes/1/essence');
+buyOption('planes/1/scope/island');
+console.log(getSelected(options));
