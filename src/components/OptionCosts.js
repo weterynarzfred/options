013ea257cc684615221c4c5887e03d741de0b200
weterynarzfred <weterynarzfred@gmail.com>
2020-02-09 14:16:58 +0100

    display costs in indicvidual currencies

--- src/components/OptionCosts.js
+++ src/components/OptionCosts.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getOption, clone } from '../functions/helpers';
+
+function getCurrencies(props, path) {
+  const currencies = clone(props.settings.currency);
+  path = path.split('/').reverse();
+  let currentPath = path.pop();
+  while (path.length > 0) {
+    currentPath += '/' + path.pop();
+    const option = getOption(currentPath, props.options);
+    if (option.optionCurrency !== undefined) {
+      Object.assign(currencies, option.optionCurrency);
+    }
+  }
+  return currencies;
+}
 
 function OptionCosts(props) {
   const option = props.option;
   if (option.type !== 'option') return null;
   if (option.cost === undefined) return null;
   const costs = [];
+  const currencies = getCurrencies(props, option.path);
+
   for (const currencySlug in option.cost) {
-    if (props.settings.currency[currencySlug] === undefined) continue;
-    const currencyName = props.settings.currency[currencySlug].name;
+    if (currencies[currencySlug] === undefined) continue;
+    const currencyName = currencies[currencySlug].name;
     const cost = option.cost[currencySlug];
     costs.push(
       <div
@@ -25,9 +42,7 @@ function OptionCosts(props) {
 }
 
 function mapStateToProps(state) {
-  return {
-    settings: state.settings,
-  };
+  return {...state};
 }
 
 export default connect(mapStateToProps)(OptionCosts);
